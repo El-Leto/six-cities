3ce2e120567d820395737a173e9f70477e87b562
El-Leto <elleto.life@gmail.com>
2021-06-01 17:48:57 +0300

    Отрисует главную страницу

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MainPage from '../main-page/main-page';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App(props) {
+  const {placeCardCount} = props;
+
+  return (
+    <MainPage
+      placeCardCount={placeCardCount}
+    />
+  );
 }
+
+App.propTypes = {
+  placeCardCount: PropTypes.number.isRequired,
+};
 
 export default App;
